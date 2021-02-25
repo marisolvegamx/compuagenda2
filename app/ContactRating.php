26c0e71e32f7c3d345429c26f1645057651fcb74
marisolvegamx <40066757+marisolvegamx@users.noreply.github.com>
2021-02-25 16:44:04 -0600

    faltaba

---
+++ app/ContactRating.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class ContactRating extends Model
+{
+    //
+    protected $table = 'ca_contactos_rating';
+    
+    protected $fillable = [
+        'ca_contactos_id', 'rating_number', 'total_points','status'
+        
+    ];
+   
+   
+    
+}
